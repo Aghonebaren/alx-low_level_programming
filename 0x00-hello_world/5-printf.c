334ba44ee2dab63ebcda98ac69548656023cb85f
Dr Josh <aladejoshua99@gmail.com>
2022-06-30 02:45:19 -0700

    Print program

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - entry point of program
+ * @void: no arguments
+ *
+ * Description: indicates start of program.
+ * Return: returns a value to exit
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
