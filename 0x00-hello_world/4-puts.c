877c73984debd1a8855d17f9a00ca94bc95301a8
Dr Josh <aladejoshua99@gmail.com>
2022-06-30 02:06:57 -0700

    My first program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
+\**
+ *main- entry point of the program
+ *return- returns 0 to standard output and exits
+ */
 
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
