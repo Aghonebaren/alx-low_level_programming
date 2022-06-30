6b1c80bc08fb26161ff4a0a11aee1c1e18c39c75
Dr Josh <aladejoshua99@gmail.com>
2022-06-30 06:28:42 -0700

    Standard error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - start of program
+ * @void: no argument
+ *
+ * Description: this function prints to standard error
+ * Return: this function returns 1
+ */
+int main(void)
+{
+	fprint(stderr, "and that piece of art is useful\" -Dora ");
+	fprint(stderr, "Korpar, 2015-10-19\n");
+	return (1);
+}
