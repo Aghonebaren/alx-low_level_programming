3efb889bd0e7ecded29014967f4c5aaea34ddca9
Dr Josh <aladejoshua99@gmail.com>
2022-06-30 02:11:23 -0700

    My first program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 \**
- *main - entry point of the program
- *return - returns 0 to standard output and exits
- */
-
+*main: entry point of the program
+*return: returns 0 to standard output and exits
+*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
