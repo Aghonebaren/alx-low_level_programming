befc76c0c6013ef1e32dc75c52ff863d3de16c6f
Dr Josh <aladejoshua99@gmail.com>
2022-07-05 12:41:09 -0700

    Main library

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 /**
  * main - funtion to print a string
+ * @void: no parameters
  *
+ * Description: prints a string of characters
  * return: Always SUCCESS
  */
 int main(void)
