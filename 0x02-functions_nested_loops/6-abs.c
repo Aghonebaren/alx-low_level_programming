be4d6c89f47db8acd67969d5fe4772c6e131d5f9
Dr Josh <aladejoshua99@gmail.com>
2022-07-05 14:06:28 -0700

    damn

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - computes absolute value of a given integer
+ * @int - test parameter
+ * Return: Always success
+ */
+int _abs(int)
+{
+	int n;
+
+	if (n < 0)
+		n = n * -1;
+	return (n);
+}
---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_last_digit - prints last digit of a given number
+ * @int: parameter
+ * Return: always success
+ */
+int print_last_digit(int)
+{
+	int n;
+	last = n % 10;
+
+	return (last)
+}
