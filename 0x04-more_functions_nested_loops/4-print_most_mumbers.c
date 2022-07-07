aa728460761842edc57aa5bd7bab1bfb139848df
Dr Josh <aladejoshua99@gmail.com>
2022-07-07 07:21:19 -0700

    Me

---
+++ 0x04-more_functions_nested_loops/4-print_most_mumbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_most_numbers - print all except 2 and 4
+ * @void: no parameters
+ *
+ * Return: return 0
+ */
+void print_most_numbers(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		if (n != 2 && n != 4)
+		{
+			_putchar(n);
+		}
+	}
+	_putchar('\n');
+
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * more_numbers - prints numbers from 0 - 14
+ * @void: no arguments
+ *
+ * Return; Always success
+ */
+void more_numbers(void)
+{
+	int n;
+	int c;
+
+	for (n = 0; n < 10; n++)
+	{
+		for (c = 0; c <= 14; c++)
+		{
+			_putchar(c);
+		}
+	_putchar(n);
+
+	}
+	_putchar('\n');
+}	
