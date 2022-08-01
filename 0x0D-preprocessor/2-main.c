35aa5ac19888b68b00e38ef40b098507d2a7b67f
Dr Josh <aladejoshua99@gmail.com>
2022-08-01 07:43:48 -0700

    Macro

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
 /**
- * main - Prints name of file code is compiled from
- * @void: No parameters
+ * main - Prints name of file code is compiled from,
+ * 	followed by a new line
  *
  * Return: Always success
  */
 
 int main(void)
 {
-	printf("%s", __FILE__);
+	printf("%s\n", __FILE__);
 
 	return (0);
 }
